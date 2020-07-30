3d68257ce94cb74bde152f880369feef1c24d17d
Limseongyun <66352922+Limseongyun@users.noreply.github.com>
2020-07-30 18:05:53 +0900

    a

--- spring_thymleaf_aop/src/mvc/log/log.java
+++ spring_thymleaf_aop/src/mvc/log/log.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 public class log {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	public void testprint() {
-		//System.out.println("aop>>test log prinst");
+		//System.out.println("aop>>test log print");
 		logger.debug("aop>>test log print");
 	}
 }
